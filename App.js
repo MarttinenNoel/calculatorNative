7482f9d7cbe6a35260797c44dbd803343b675a13
MarttinenNoel <noelmarttinen@gmail.com>
2021-01-27 16:26:42 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = React.useState('');
+  const [text1, setText1] = React.useState('');
+  const [result, setResult] = React.useState('');
+
+  const addNumbers = () => {
+    setResult((parseInt(text) + parseInt(text1)));
+  }
+  const subNumbers = () => {
+    setResult(parseInt(text) - parseInt(text1));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput
+      keyboardType={"numeric"}
+      style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={text => setText(text)}
+      value={text}
+      />
+      <TextInput
+      keyboardType={"numeric"}
+      style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={text1 => setText1(text1)}
+      value={text1}
+      />
+      <View style={{flex: 0, flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-around'}}>
+      <Button onPress={addNumbers} title="+" /> 
+      <Button onPress={subNumbers} title="-" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
